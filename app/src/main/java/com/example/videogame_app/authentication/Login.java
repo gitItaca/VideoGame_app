78eeaaf10c428143172c913409176b83ba19aa73
Loreto <0314lsolanes@e-itaca.es>
2021-05-10 20:48:04 +0200

    Añadido detalle del progressBar

--- app/src/main/java/com/example/videogame_app/authentication/Login.java
+++ app/src/main/java/com/example/videogame_app/authentication/Login.java
@@ -68,8 +68,8 @@ public class Login extends AppCompatActivity {
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         }else{
                             Toast.makeText(Login.this, "Error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
                         }
-
                     }
                 });
             }
