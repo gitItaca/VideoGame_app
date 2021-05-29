1bc8b575d31ca16b4632094b54b19f73376c7d73
Loreto <0314lsolanes@e-itaca.es>
2021-05-29 20:23:49 +0200

    Guarda, lee y borra los videojuegos de mi base de datos.
    En la pantalla de detalle hay un botón para guardar los videojuegos en tu lista o borrarlo si ya está en ella.

--- app/src/main/java/com/example/videogame_app/VideogameDetailActivity.java
+++ app/src/main/java/com/example/videogame_app/VideogameDetailActivity.java
@@ -1,13 +1,10 @@
 package com.example.videogame_app;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,27 +12,23 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.bumptech.glide.Glide;
 import com.example.videogame_app.interfaces.VideojuegoAPI;
-import com.example.videogame_app.models.UserModel;
 import com.example.videogame_app.models.VideogameModel;
-import com.example.videogame_app.models.VideojuegoRespuesta;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
 import org.jsoup.Jsoup;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -45,12 +38,11 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import static android.os.Build.VERSION_CODES.M;
-
 public class VideogameDetailActivity extends AppCompatActivity {
 
    private Bundle bundle;
    private Retrofit retrofit;
+   private boolean inLoveList;
    private static final String TAG = "VideogameDetailActivity";
 
    FirebaseAuth fbAuth;
@@ -69,29 +61,52 @@ public class VideogameDetailActivity extends AppCompatActivity {
         TextView description = findViewById(R.id.descriptionVideogameDetail);
         TextView webText = findViewById(R.id.nameWebsiteVideogameDetail);
 
-        //___Inicializo la base de datos
+        Drawable cruxImg = ResourcesCompat.getDrawable(getResources(), R.drawable.close, null);
+        Drawable heartImg = ResourcesCompat.getDrawable(getResources(), R.drawable.heart, null);
+
+        //___Recojo el id del MainActivity.
+        bundle = getIntent().getExtras();
+        int id = bundle.getInt("id_videogame_selected");
+        String idVideogameToString = Integer.toString(id);
+
+        //___Inicializo la base de datos.
         fbAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
         String userId = fbAuth.getCurrentUser().getUid();
-        //___Guardo los videojuegos con firebase
+        DocumentReference docRefListaDeseo = db.document(userId+"/listaDeseo");
+        //inLoveList = false;
+
+        //___Leo la lista de mi base de datos y compruebo si el videojuego está.
+        docRefListaDeseo.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    Object savedLoveListGame = documentSnapshot.get(idVideogameToString);
+                    if(savedLoveListGame == null){
+                        buttonSaveLove.setImageDrawable(heartImg);
+                        inLoveList = false;
+                    }else{
+                        buttonSaveLove.setImageDrawable(cruxImg);
+                        inLoveList = true;
+                    }
+                }else{
+                    Toast.makeText(VideogameDetailActivity.this, "El documento no existe", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //___Guardo los videojuegos con firebase.
         Map<String, Object> videojuegoMap = new HashMap<>();
-        Map<String, Object> userMap = new HashMap<>();
 
-        //___Recojo el id del MainActivity
-        bundle = getIntent().getExtras();
-        int id = bundle.getInt("id_videogame_selected");
-        String idToAPI = Integer.toString(id);
-        //Log.d(TAG, String.valueOf(id));
-
-        //___Creo el intent y le asocio la pagina de la web
+        //___Creo el intent y le asocio la pagina de la web.
         Intent intentWeb = new Intent(this, WebsiteVideogameActivity.class);
 
-        //___Llamo a la API
+        //___Llamo a la API.
         retrofit = new Retrofit.Builder().baseUrl("https://api.rawg.io/")
                 .addConverterFactory(GsonConverterFactory.create()).build();
 
         VideojuegoAPI videojuegoAPI = retrofit.create(VideojuegoAPI.class);
-        Call<VideogameModel> videogameDetailCall = videojuegoAPI.find(idToAPI);
+        Call<VideogameModel> videogameDetailCall = videojuegoAPI.find(idVideogameToString);
 
         videogameDetailCall.enqueue(new Callback<VideogameModel>() {
             @Override
@@ -136,54 +151,29 @@ public class VideogameDetailActivity extends AppCompatActivity {
             }
         });
 
-        //___Boton para enviar el videojuego a mi lista y guardarlo
+        //___Boton para enviar el videojuego a mi lista de deseos y guardarlo o borrarlo.
         buttonSaveLove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                userMap.put("IdUser", userId);
 
-                //___Meto la info que quiero guardar en el objeto videojuegoMap
-                //videojuegoMap.put("IdGame", videojuego.getId());
-                UserModel userm = new UserModel();
-                userm.setIdUser(userId);
-                ArrayList<Integer> listaDeseo = new ArrayList<>();
-                listaDeseo.add(videojuego.getId());
-                userm.setListaDeseo(listaDeseo);
+                if(inLoveList==false) {
+                    //___Add a new document with a generated ID
+                    videojuegoMap.put(idVideogameToString, videojuego);
 
-//                db.collection("users")
-//                        .document(userId)
-//                        .set(userm)
-//                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                            @Override
-//                            public void onSuccess(DocumentReference documentReference) {
-//                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-//                            }
-//                        })
-//                        .addOnFailureListener(new OnFailureListener() {
-//                            @Override
-//                            public void onFailure(@NonNull Exception e) {
-//                                Log.w(TAG, "Error adding document", e);
-//                            }
-//                        });
-                //___Add a new document with a generated ID
-                db.collection("videojuegos")
-                        .add(videojuegoMap)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Error adding document", e);
-                            }
-                        });
+                    docRefListaDeseo.set(videojuegoMap, SetOptions.merge());
+
+                    buttonSaveLove.setImageDrawable(cruxImg);
+                }else {
+                    //___Delete
+                    docRefListaDeseo.update(idVideogameToString, FieldValue.delete());
+
+                    buttonSaveLove.setImageDrawable(heartImg);
+                }
+                inLoveList=!inLoveList;
             }
         });
 
-        //___Boton para volver a la pagina principal
+        //___Boton para volver a la pagina principal.
         Intent intentVolver = new Intent(this, MainActivity.class);
         buttonReturn.setOnClickListener(new View.OnClickListener() {
             @Override
