fe4c0d60a54555db3f75f049606710c796372505
Loreto <0314lsolanes@e-itaca.es>
2021-06-05 21:36:32 +0200

    He cambiado algo el aspecto de la aplicación

--- app/src/main/java/com/example/videogame_app/MainActivity.java
+++ app/src/main/java/com/example/videogame_app/MainActivity.java
@@ -16,8 +16,6 @@ import com.example.videogame_app.interfaces.VideojuegoAPI;
 import com.example.videogame_app.models.VideogameModel;
 import com.example.videogame_app.models.VideojuegoRespuesta;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
